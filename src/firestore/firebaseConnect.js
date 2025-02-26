e0c8519afe9dc0360a78460173e2072d46b1576b
FabiodAgostino <83577536+FabiodAgostino@users.noreply.github.com>
2025-02-26 14:31:27 +0100

    Update firebaseConnect.js
    hide key

--- src/firestore/firebaseConnect.js
+++ src/firestore/firebaseConnect.js
@@ -7,13 +7,13 @@ var db;
 async function connectFirebase()
     {
         const firebaseConfig = {
-            apiKey: "AIzaSyBNWbqdAaq44dWHELm2m1jU0xY6graA9uo",
+            apiKey: "",
             authDomain: "rotiniel-35c5b.firebaseapp.com",
             projectId: "rotiniel-35c5b",
             storageBucket: "rotiniel-35c5b.appspot.com",
-            messagingSenderId: "546264380520",
-            appId: "1:546264380520:web:b09f4bc5e62c611818f7dd",
-            measurementId: "G-VXQ0LYQVDB"
+            messagingSenderId: "",
+            appId: "",
+            measurementId: ""
         };
         
         const app = initializeApp(firebaseConfig);
